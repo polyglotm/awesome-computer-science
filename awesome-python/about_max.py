3963d4100782a689f1dc1e4539987bd93e3c73c8
Jay Lee <polyglot.m@gmail.com>
2021-10-11 02:20:32 +0900

    feat(awesome-python/about_max.py): add about max

---
+++ awesome-python/about_max.py
@@ -0,0 +1,20 @@
+def test_max():
+    # max function return maximum value of iterable object
+
+    value = [1, 2, 3, 4, 5]
+    assert max(value) == 5
+
+    # NOTE: 'str' type is iterable.
+    value = '12345'
+    assert max(value) == '5'
+
+    value = ['a', 'b', 'c', 'd', 'e']
+    assert max(value) == 'e'
+
+    # NOTE: 'str' type is iterable.
+    value = 'abcde'
+    assert max(value) == 'e'
+
+    # # TypeError: 'int' object is not iterable
+    # list = 12345
+    # assert max(list) == 12345
